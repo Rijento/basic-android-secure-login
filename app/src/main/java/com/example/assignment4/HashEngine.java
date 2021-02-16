a36b5d09902330e2252bee86cc735efcb2148fe2
rijento <rijento@gmial.com>
2021-02-16 14:37:57 -0500

    added hashing engine

---
+++ app/src/main/java/com/example/assignment4/HashEngine.java
@@ -0,0 +1,28 @@
+package com.example.assignment4;
+
+import java.security.NoSuchAlgorithmException;
+import java.security.SecureRandom;
+import java.security.spec.InvalidKeySpecException;
+
+import javax.crypto.SecretKeyFactory;
+import javax.crypto.spec.PBEKeySpec;
+
+public class HashEngine {
+    private static final int HASH_BYTE_SIZE = 512;
+    private static final int PBKDF2_ITERATIONS = 1000;
+    private static final int SALT_BYTE_SIZE = 32;
+    private static SecureRandom random;
+    HashEngine() {
+        random = new SecureRandom();
+    }
+
+    public byte[][] saltAndHash(char[] input) throws NoSuchAlgorithmException, InvalidKeySpecException {
+        byte salt[] = new byte[SALT_BYTE_SIZE];
+        random.nextBytes(salt);
+
+        PBEKeySpec spec = new PBEKeySpec(input, salt, PBKDF2_ITERATIONS, HASH_BYTE_SIZE);
+        SecretKeyFactory skf = SecretKeyFactory.getInstance("PBKDF2WithHmacSHA512");
+        byte[] hash = skf.generateSecret(spec).getEncoded();
+        return new byte[][]{salt, hash};
+    }
+}
